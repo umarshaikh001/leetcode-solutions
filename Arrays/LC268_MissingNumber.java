583188e8324424ac852c6753b4be147d72159a37
umarshaikh001 <umarshaikh01001@gmail.com>
2026-07-05 21:58:08 +0530

    Add LeetCode 268 Missing Number

---
+++ Arrays/LC268_MissingNumber.java
@@ -0,0 +1,28 @@
+public class LC268_MissingNumber {
+
+     static int findMissingElement(int nums[]){
+
+        int n = nums.length;
+        int missing = 0;
+
+
+        for(int i : nums) {
+            missing ^= i;
+
+        }
+
+        for(int i=1;i<=n;i++) {
+            missing ^= i;
+        }
+        return missing;
+    }
+
+    public static void main(String[] args) {
+
+         int arr[] = {0,1,2,3,5};
+
+        System.out.println(findMissingElement(arr));
+        
+    }
+    
+}
