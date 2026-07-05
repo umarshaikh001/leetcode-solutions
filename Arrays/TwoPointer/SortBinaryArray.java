54b0ed8596ca7f3700eb2c0f06dee62987076243
umarshaikh001 <umarshaikh01001@gmail.com>
2026-07-05 20:30:54 +0530

    Add Sort Binary Array

---
+++ Arrays/TwoPointer/SortBinaryArray.java
@@ -0,0 +1,37 @@
+package TwoPointer;
+
+public class SortBinaryArray {
+
+     static int[] sortBinaryArray(int nums[]) {
+
+        int n = nums.length;
+        int start = 0;
+        int end = n -1;
+
+        while (start <= end) {
+
+            if(nums[start] == 1 && nums[end] == 0) {
+                nums[start] =0;
+                nums[end] = 1;
+            }
+
+            if(nums[start] == 0) {
+                
+                start++;
+            }
+
+            if(nums[end] == 1) {
+                
+                end--;
+
+            }
+            
+        }
+        return nums;
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
