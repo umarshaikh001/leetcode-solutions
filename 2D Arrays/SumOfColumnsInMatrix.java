75af2e16a627662ff3a532b8b789e23d0a74b2cd
umarshaikh001 <umarshaikh01001@gmail.com>
2026-07-12 18:13:23 +0530

    Add Sum Of Columns In Matrix (Similar to Richest Customer Wealth)

--- 2D Arrays/SumOfColumnsInMatrix.java
+++ 2D Arrays/SumOfColumnsInMatrix.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class SumOfColumnsInMatrix {
 
-     static List<Integer> sumOfColumns(int matrix[][]) {
+    static List<Integer> sumOfColumns(int matrix[][]) {
         List<Integer> output = new ArrayList<>();
 
         int n = matrix.length;
@@ -19,7 +18,10 @@ public class SumOfColumnsInMatrix {
         return output;
     }
     public static void main(String[] args) {
-        
+         int nums[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.print(sumOfColumns(nums) + " ");
+       
+       
         
     }
     
---
+++ leetcodeExtra/LC1672_RichestCustomerWealth.java
@@ -0,0 +1,27 @@
+public class LC1672_RichestCustomerWealth {
+
+     static int   maximumWealth(int matrix[][]) {
+        
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int maxValue = Integer.MIN_VALUE;
+
+         for(int row=0;row<n;row++) {
+            
+            int sum =0;
+            for(int col=0;col<m;col++) {
+             sum += matrix[row][col];
+            }
+            
+            maxValue = Math.max(maxValue, sum);
+        }
+        
+        return maxValue;
+    }
+    public static void main(String[] args) {
+         int nums[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.print(maximumWealth(nums) + " ");
+        
+    }
+    
+}
