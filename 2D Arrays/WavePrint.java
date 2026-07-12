68842b365dfaef06db048199b6cf08e57c64c243
umarshaikh001 <umarshaikh01001@gmail.com>
2026-07-12 17:45:47 +0530

    Add Wave Print

--- 2D Arrays/WavePrint.java
+++ 2D Arrays/WavePrint.java
@@ -27,6 +27,8 @@ public class WavePrint {
         return output;
         
     }
+
+   
     public static void main(String[] args) {
           int nums[][] = {{1,2,3},{4,5,6},{7,8,9}};
         System.out.print(wavePrint(nums) + " ");
